eb62d58a1af71a9e734760cb2d0bf558518b3606
DkdEviL <deepaknardia98@gmail.com>
2020-07-30 20:48:47 +0530

    Created Component for Header Section

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HeaderCanvas from './components/headerCanvas';
 
 function App() {
   return (
     <div className="App">
       <div className="mainBody">
-        <div id="canvas">
-          <header id="header">
-            <div id="logo" className="site-title">
-              <h1 className="logo">
-                <a href="#">Deepak Kumar</a>
-              </h1>
-            </div>
-          </header>
-          <div class="menuNav">
-            <nav>
-              <ul>
-                <li><a href="#">About this site</a></li>
-                <li><a href="#">About Me</a></li>
-              </ul>
-            </nav>
-          </div>
-        </div>
+        <HeaderCanvas />
         <div>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
---
+++ src/components/headerCanvas.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './headerCanvas.css';
+
+function headerCanvas() {
+    return (
+        <div id="canvas">
+          <header id="header">
+            <div id="logo" className="site-title">
+              <h1 className="logo">
+                <a href="#">Deepak Kumar</a>
+              </h1>
+            </div>
+          </header>
+          <div class="menuNav">
+            <nav>
+              <ul class="mainNav">
+                <li><a href="#">About this site</a></li>
+                <li><a href="#">About Me</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+    )
+}
+
+export default headerCanvas;
