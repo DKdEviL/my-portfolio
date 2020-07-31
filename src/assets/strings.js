5a5850c0c16dccdb4dcca4380a23eacb44928246
DkdEviL <deepaknardia98@gmail.com>
2020-07-31 16:47:56 +0530

    Image.js file is replaced with strings.js file which includes urls for various links added in the site

---
+++ src/assets/strings.js
@@ -0,0 +1,11 @@
+const imageUrls = {
+    imageUrl: {
+        myImage: "https://lh3.googleusercontent.com/iY3Bwp1q38mdPjgAxPMVoxfeDhaJIwhTkxAU_97WwGH46fFKwe1ahJ08ZvSST26yPgQOEqBYG_Dlgm-R5VNihUTO3B52IV7Jhaaagw-cKgV02fEL1iwsxoA5BCRVNzuHe4-zYvwIf3eB_N651aX4OMw1stdN4_jNMIL_cjkskNkaoi0IrX3x7cOp3U5zQ1HynO4rX_jjuV80KvwEf386nnoUDeDlfJ0Fu0zcdUo4_l3vQyPvdXEiYV7bh2J6QwgpFERl4MWYY621UDHvWOGe656wZMcyKYsPKvoVkdrC5A8pAiJtB7jsNxC61WX9eFj80gR5gpiFMiQsExflYkdD34dbcvzS-lXiq1POU5P3rAz3hnGH692xKRkx8Bs5xeI3udi-CtJo_2h_q0fXFm_NPsDMAm8JUrzW7sF7yEnbG9eCuOh1Bt_CmzOz7W89h4jGk2JArFSQ2Hl6wYagB08zUadEPFYiBLxARBNTkxua66e4uWkSAO8j9InUz1HFPNppVRNsYIbmHuBx-EPghtpONtYrEGi-otJKCynuYoN63LfJK3pT5zPNwsDgQb1QizNq5IARChY6-tX1f-BmRoSfnQvHUMINLSShV4D-OjFmUEusOLrMU3iqukhAz8S4A_04f4pfMQ2knjRIU0PQgcbqbil4jDJy9bCT-KG69fMgpdYlai5wvQBc5kbQC2tQyA=w811-h608-no?authuser=2"
+    },
+    urls: {
+        uniLink: "http://www.dcrustm.ac.in/",
+        hckrUrl: "https://www.hackerearth.com/@deepaknardia98"
+    }
+};
+
+export default imageUrls;
