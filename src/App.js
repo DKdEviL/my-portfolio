faecb845e78f92e006e89a4418cd8ebe5d43f7ba
DkdEviL <deepaknardia98@gmail.com>
2020-07-31 16:42:49 +0530

    Description Component Added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HeaderCanvas from './components/headerCanvas';
-import imageUrls from './assets/image';
+import AboutMe from './components/aboutMe/aboutMeComponent'
+
+
 function App() {
   return (
     <div className="App">
       <div className="mainBody">
         <HeaderCanvas />
-        <div>
-          <img src={imageUrls.imageUrl.myImage}></img>
-        
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-          </p>
-        </div>
+        <AboutMe />
       </div>
       
       
