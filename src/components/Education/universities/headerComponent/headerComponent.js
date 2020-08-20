434efb6bcea7429796b05a5fc84a192277ba5304
DkdEviL <deepaknardia98@gmail.com>
2020-08-21 00:45:17 +0530

    Card Content Header Component modified and updated to Reusable Component

--- src/components/Education/universities/headerComponent/headerComponent.js
+++ src/components/Education/universities/headerComponent/headerComponent.js
@@ -4,7 +4,7 @@ import links from '../../../../assets/strings'
 function Header(props) {
     return (
         <div className="degree-header" >
-            <h4 onClick={props.clicked}><span>{props.course}</span></h4>
+            <h4 className="card-content-header" onClick={props.clicked}><span>{props.course}</span></h4>
             <div className="image-button-container" onClick={props.clicked}>
                 {
                     props.show ?
@@ -15,32 +15,7 @@ function Header(props) {
             </div>
             {
                 props.show ?
-                <div className="degree">
-                    <div>
-                        <img style={{padding: 0, width: "100%", height: "100%"}} src={links.imageUrl.icons.student}/>
-                    </div>
-                    <div>
-                        <strong>
-                            <a href={links.urls.dcrust} className="big-screen clg">Deenbandhu Chhotu Ram University of Science and Technology</a>
-                            <a href={links.urls.dcrust} className="small-screen clg">DCRUST</a>
-                            <p style={{margin: 0}}>(2016 - 2020)</p>
-                        </strong>
-                        <div class="pointer">
-                            <h5>CGPA:</h5>
-                            <h5>6.85</h5>
-                        </div>
-                        <div class="pointer">
-                            <h5>Course:</h5>
-                            <h5 className="big-screen">Bachelor of Technology</h5>
-                            <h5 className="small-screen">B.Tech</h5>
-                        </div>
-                        <div class="pointer">
-                            <h5>Major:</h5>
-                            <h5 className="big-screen">Computer Science and Engineering</h5>
-                            <h5 className="small-screen">CSE</h5>
-                        </div>
-                    </div>
-                </div> :
+                    props.children :
                 <div style={{display: "none"}}></div>
             }
             
