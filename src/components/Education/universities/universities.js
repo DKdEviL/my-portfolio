79eb47d7d38d43a3ef62cb9619e7409f183b8dcc
DkdEviL <deepaknardia98@gmail.com>
2020-08-21 00:46:17 +0530

    Some reusable components are used properly

--- src/components/Education/universities/universities.js
+++ src/components/Education/universities/universities.js
@@ -23,7 +23,34 @@ class Universities extends Component {
         return (
             <Card imageUrl={links.imageUrl.icons.university} headingText="Education">
                 <div className="card-item-container">
-                    <Header course={"Bachelor's"} clicked={this.clickHandler} show={this.state.show}/>
+                    <Header course={"Bachelor's"} clicked={this.clickHandler} show={this.state.show}>
+                    <div className="degree">
+                    <div>
+                        <img style={{padding: 0, width: "100%", height: "100%"}} src={links.imageUrl.icons.student}/>
+                    </div>
+                    <div>
+                        <strong>
+                            <a href={links.urls.dcrust} className="big-screen clg">Deenbandhu Chhotu Ram University of Science and Technology</a>
+                            <a href={links.urls.dcrust} className="small-screen clg">DCRUST</a>
+                            <p style={{margin: 0}}>(2016 - 2020)</p>
+                        </strong>
+                        <div class="pointer">
+                            <h5>CGPA:</h5>
+                            <h5>6.85</h5>
+                        </div>
+                        <div class="pointer">
+                            <h5>Course:</h5>
+                            <h5 className="big-screen">Bachelor of Technology</h5>
+                            <h5 className="small-screen">B.Tech</h5>
+                        </div>
+                        <div class="pointer">
+                            <h5>Major:</h5>
+                            <h5 className="big-screen">Computer Science and Engineering</h5>
+                            <h5 className="small-screen">CSE</h5>
+                        </div>
+                    </div>
+                </div>
+                    </Header>
                 </div>
             </Card>
         )
--- src/components/work/work.js
+++ src/components/work/work.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../card/card';
+import CardHeader from '../Education/universities/headerComponent/headerComponent'
 import links from '../../assets/strings'
 
-function Work() {
+class Work extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            show : false,
+            showWeb: false
+        }
+    }
+    clickhandlerAndroid = () => {
+        this.setState({show: !this.state.show})
+    }
+    clickhandlerWeb = () => {
+        this.setState({showWeb: !this.state.showWeb})
+    }
+    render(){
+
     return (
         <div>
             <p>In Development...👨‍💻👨‍💻👨‍💻</p>
             
-            <Card imageUrl={links.imageUrl.icons.projects} headingText="Learning Projects"/>
+            <Card imageUrl={links.imageUrl.icons.projects} headingText="Learning Projects">
+                <div className="demo-card-content">
+                    <CardHeader
+                        course="Android Projects"
+                        clicked={this.clickhandlerAndroid}
+                        show={this.state.show} 
+                    >
+                        <div>
+                            <p>Android Projects Here...</p> 
+                        </div>
+                    </CardHeader>
+                    <CardHeader
+                        course="Front-end Projects"
+                        clicked={this.clickhandlerWeb}
+                        show={this.state.showWeb} 
+                    >
+                        <div>
+                            <p>Web  Projects Here...</p>
+                        </div>
+                    </CardHeader>
+                </div>
+            </Card>
+
             <Card imageUrl={links.imageUrl.icons.work} headingText="Work Experience">
                 <div className="demo-card-content">
                     <p>Open to face your challenge for opportunity.</p>
@@ -18,6 +56,7 @@ function Work() {
             
         </div>
     )
+    }
 }
 
 export default Work;
